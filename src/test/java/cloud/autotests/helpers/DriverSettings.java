3f97340d2735291e2c733c50927873e37ba5a18f
Svetlana <89111378147@yandex.ru>
2022-02-17 00:05:03 +0300

    tests added

--- src/test/java/cloud/autotests/helpers/DriverSettings.java
+++ src/test/java/cloud/autotests/helpers/DriverSettings.java
@@ -15,7 +15,7 @@ public class DriverSettings {
         Configuration.browserVersion = Project.config.browserVersion();
         Configuration.browserSize = Project.config.browserSize();
         Configuration.baseUrl = "https://www.petshop.ru/";
-        Configuration.timeout = 12000;
+        Configuration.timeout = 3000;
 
         DesiredCapabilities capabilities = new DesiredCapabilities();
         ChromeOptions chromeOptions = new ChromeOptions();
