7be8db7d493e46095439094df45f4d16c1d5c7b3
Svetlana <89111378147@yandex.ru>
2022-02-16 23:37:56 +0300

    tests added

---
+++ src/test/java/cloud/autotests/tests/ConsoleLogTest.java
@@ -0,0 +1,30 @@
+package cloud.autotests.tests;
+
+import cloud.autotests.helpers.DriverUtils;
+import io.qameta.allure.Description;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import static com.codeborne.selenide.Selenide.*;
+import static io.qameta.allure.Allure.step;
+import static org.assertj.core.api.Assertions.assertThat;
+
+
+public class ConsoleLogTest extends TestBase {
+
+
+    @Test
+    @Description("works only for web version, does not work for mobile version of  site")
+    @DisplayName("Page console log should not have errors")
+    void consoleShouldNotHaveErrorsTest() {
+        step("Open url", () ->
+                open(" "));
+
+        step("Console logs should not contain text 'SEVERE'", () -> {
+            String consoleLogs = DriverUtils.getConsoleLogs();
+            String errorText = "SEVERE";
+
+            assertThat(consoleLogs).doesNotContain(errorText);
+        });
+    }
+}
---
+++ src/test/java/cloud/autotests/tests/SearchTests.java
@@ -0,0 +1,44 @@
+package cloud.autotests.tests;
+
+import cloud.autotests.helpers.DriverUtils;
+import io.qameta.allure.Description;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import static com.codeborne.selenide.Condition.text;
+import static com.codeborne.selenide.Condition.visible;
+import static com.codeborne.selenide.Selenide.*;
+import static com.codeborne.selenide.files.DownloadActions.click;
+import static io.qameta.allure.Allure.step;
+import static org.assertj.core.api.Assertions.assertThat;
+
+
+public class SearchTests extends TestBase {
+    @Test
+    @Description("works only for web version, does not work for mobile version of  site")
+    @DisplayName("Common page elements for PS")
+    void searchTest() {
+        step("Open url", () ->
+                open(""));
+        step("check city hint", () -> {
+            $(".MuiInputBase-input").setValue("корм для кошек").pressEnter();
+            $(".product-list:nth-child(1)").shouldHave(text("кошек"));
+
+        });
+    }
+
+    @Test
+    @Description("works only for web version, does not work for mobile version of  site")
+    @DisplayName("Common page elements for PS")
+    void searchEmptyTest() {
+        step("Open url", () ->
+                open(""));
+        step("check city hint", () -> {
+            $(".MuiInputBase-input").setValue(" ").pressEnter();
+            $(".errortext").shouldHave(text("Пустой поисковый запрос"));
+
+        });
+    }
+
+
+}
